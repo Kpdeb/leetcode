3aa349f757b1bb619a3d466d686c1a9482de30a3
kumar priyanshu <kumarpriyanshu5636@gmail.com>
2026-09-22 10:56:43 +0530

    Time: 5 ms (52.45%), Space: 12.6 MB (0.57%) - LeetHub

---
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.py
@@ -0,0 +1,12 @@
+class Solution:
+    def finalPrices(self, prices):
+        n = len(prices)
+        answer = prices[:]
+
+        for i in range(n):
+            for j in range(i + 1, n):
+                if prices[j] <= prices[i]:
+                    answer[i] = prices[i] - prices[j]
+                    break
+
+        return answer
